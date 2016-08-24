5f9c28dffd9dc81b111fbccd7daf28e60c538c66
Michael Wuergler <wuergler@gmail.com>
2016-08-24 09:50:14 -0400

    init

---
+++ test/test.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import test from 'ava';
+import fn from '../';
+
+test('converts srt to obj', async t => {
+	const data = await fn(path.resolve('./fixtures/subtitles.srt'));
+
+	const firstEntry = data[0];
+	t.is(firstEntry.index, '1');
+	t.is(firstEntry.start, '00:00:55,880');
+	t.is(firstEntry.end, '00:00:57,670');
+	t.is(firstEntry.text, 'Line one');
+});
+
+test('grabs multiline subs', async t => {
+	const data = await fn(path.resolve('./fixtures/subtitles.srt'));
+
+	const secondEntry = data[1];
+	t.is(secondEntry.text, 'Line two\nLine three\nLine four\nLine five');
+});
